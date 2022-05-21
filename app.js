475e169347b74fb40e0320b4cac54ce16efaaead
Тимур <68272322+Owe7x@users.noreply.github.com>
2022-05-21 16:23:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -28,10 +28,20 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(requestLogger);
 
-app.use(cors({
-  origin: 'https://api.owe7x.nomoreparties.sbs',
-  credentials: true,
-}));
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Credentials', true);
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+});
 
 app.get('/crash-test', () => {
   setTimeout(() => {
